6e81be996dc156a1a35b65eb74e6ee41db2b7966
SamuelJamesDev <91440489+SamuelJamesDev@users.noreply.github.com>
2021-09-26 17:28:14 -0700

    Create launch.bat

---
+++ launch.bat
@@ -0,0 +1,29 @@
+@echo off
+color 0B
+echo 
+echo     ========================
+echo     *                      *
+echo     * ==================== *
+echo     * =                  = * 
+echo     * =                  = *  [********************************]
+echo     * =  0            0  = *  [--------------------------------]
+echo     * =     _ __ _ _     = *  [Please Log Your Daily Statistics]
+echo     * =     -_    _-     = * /[--------------------------------]
+echo     * =       ----       = * \[********************************]
+echo     * =                  = * 
+echo _   * =                  = *   _
+echo :   * ==================== *   :
+echo ::  *                      *  ::
+echo ::  *    __       ____  __ *  ::
+echo  :::*  ------      --   -- *:::
+echo     *    --        ______  *
+echo     *               ====   *
+echo     *                      *
+echo     -___  ____________  ___-
+echo         ==            ==        
+echo         ==            ==        
+echo         ==            ==        
+echo        _==            ==_
+ping 127.0.0.1 -n 3 > nul
+C:\Users\samjm\Anaconda3\python.exe C:\Users\samjm\Desktop\Personal_Projects\DailyLogData\logger.py        
+pause
